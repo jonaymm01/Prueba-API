c6b4bb94af81c08e9104d0fbbcaac149dc8b492c
Jonay <alu0101347141@ull.edu.es>
2022-04-19 17:40:04 +0100

    Inicializado ejercicio

--- src/ejercicio.ts
+++ src/ejercicio.ts
@@ -1,85 +1,9 @@
 /**
- * Clase RandomNumber
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
  */
-export class RandomNumber {
-  private static randomNumberInstance: RandomNumber;
-  /**
-   * Constructor clase RandomNumber
-   */
-  private constructor() {
-  }
-  /**
-   * "Getter" de la única instancia de la clase singleton
-   * @return {RandomNumber}
-   */
-  public static getRandomNumberInstance(): RandomNumber {
-    if (!RandomNumber.randomNumberInstance) {
-      RandomNumber.randomNumberInstance = new RandomNumber();
-    }
-    return RandomNumber.randomNumberInstance;
-  }
-  /**
-   * Número aleatorio entre 0 y 1
-   * @return {number}
-   */
-  zeroToOne(): number {
-    return Math.random();
-  }
-  /**
-   * Número flotante aleatorio entre un número min y otro max
-   * @param {number} min
-   * @param {number} max
-   * @return {number}
-   */
-  minToMaxFloat(min: number, max: number): number {
-    return Math.random() * (max - min) + min;
-  }
-  /**
-   * Número entero aleatorio entre un número min y otro max
-   * @param {number} min
-   * @param {number} max
-   * @return {number}
-   */
-  minToMaxInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-}
-
-/**
- * Clase NumberCollection
- */
-export class NumberCollection implements Iterable<number> {
-  private numeros: Set<number>;
-
-  /**
-   * Constructor de la clase NumberCollection
-   * @param {number[]} numeros
-   */
-  constructor(numeros: number[]) {
-    this.numeros = new Set(numeros);
-  }
-
-  /**
-   * Función para añadir un número a la colección
-   * @param {number} newNumber
-   */
-  addNumber(newNumber: number) {
-    this.numeros.add(newNumber);
-  }
-
-  /**
-   * Getter de la cantidad de números de la colección
-   * @return {number}
-   */
-  getNumberOfNumbers(): number {
-    return this.numeros.size;
-  }
-
-  /**
-   * Propiedad symbol.iterator
-   * @return {Iterator<number>}
-   */
-  [Symbol.iterator](): Iterator<number> {
-    return this.numeros.values();
-  }
+export function addNumber(a: number, b: number): number {
+  return a + b;
 }
--- tests/ejercicio.spec.ts
+++ tests/ejercicio.spec.ts
@@ -1,43 +1,12 @@
 import 'mocha';
 import {expect} from 'chai';
-import {RandomNumber} from '../src/ejercicio';
-import {NumberCollection} from '../src/ejercicio';
-
+import {addNumber} from '../src/ejercicio';
 
 describe('RandomNumber', () => {
-  it('Debe existir una clase RandomNumber', () => {
-    expect(RandomNumber).to.exist;
+  it('Debe existir una función para sumar dos números', () => {
+    expect(addNumber).to.exist;
   });
-  const numero1 = RandomNumber.getRandomNumberInstance();
-  const numero2 = RandomNumber.getRandomNumberInstance();
-  it('Es una clase singleton', () => {
-    expect(numero1).to.deep.equal(numero2);
-  });
-  it('zeroToOne() genera un numero entre 0 y 1', () => {
-    expect(numero1.zeroToOne()).to.be.greaterThan(0);
-    expect(numero1.zeroToOne()).to.be.lessThan(1);
-  });
-  it('minToMaxFloat(min, max) genera un numero entre min y max', () => {
-    expect(numero1.minToMaxFloat(3, 8)).to.be.greaterThanOrEqual(3);
-    expect(numero1.minToMaxFloat(3, 8)).to.be.lessThanOrEqual(8);
-  });
-  it('minToMaxInt(min, max) genera un numero entre min y max', () => {
-    expect(numero1.minToMaxInt(3, 8)).to.be.greaterThanOrEqual(3);
-    expect(numero1.minToMaxInt(3, 8)).to.be.lessThanOrEqual(8);
-  });
-});
-
-describe('NumberCollection', () => {
-  it('Debe existir una clase NumberCollection', () => {
-    expect(NumberCollection).to.exist;
-  });
-  const numero = RandomNumber.getRandomNumberInstance();
-  const collection = new NumberCollection([numero.zeroToOne(), numero.minToMaxFloat(2, 4), numero.zeroToOne()]);
-  it('La colección tiene tres números', () => {
-    expect(collection.getNumberOfNumbers()).to.be.equal(3);
-  });
-  it('Se puede añadir un número a la colección', () => {
-    collection.addNumber(numero.zeroToOne());
-    expect(collection.getNumberOfNumbers()).to.be.equal(4);
+  it('2 + 2 debe devolver 4', () => {
+    expect(addNumber(2, 2)).to.be.equal(4);
   });
 });
